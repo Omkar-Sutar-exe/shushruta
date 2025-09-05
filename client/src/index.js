632c7768b291c5e5e4b3c6ab15d739aa2e879baf
Omkar Sutar <itsofficialomkar@gmail.com>
2025-09-05 14:43:37 +0530

    project upload

---
+++ client/src/index.js
@@ -0,0 +1,32 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+// serviceWorker.unregister();
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const root = document.getElementById("root");
+
+if (root) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    root
+  );
+} else {
+  console.error("Root element not found");
+}
+
+serviceWorker.unregister();
